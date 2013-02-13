96a3356cb85d9107da15d2e5397aceb14b88a160
Deon 'PrinceAMD' Thomas <PrinceAMD.Elive@Gmail.com>
2013-02-13 06:48:45 -0400

    E17 - Menu Feature + News + Changelog
    Efreet Feature Exp 01
    
    Efreet feature 02
    
    Fixed form zmike, seems efreet is quite sloopy with specification and exec can be missing and it will still add it, so i needed to do null check there, and \0 is not space, i feel stupid
    
    added public cache func

--- src/bin/e_config.h
+++ src/bin/e_config.h
@@ -407,6 +407,7 @@ struct _E_Config
       const char *desklock_layout;
    } xkb;
    
+   Eina_List  *menu_applications;
    unsigned char exe_always_single_instance;
    int           use_desktop_window_profile; // GUI
    int post_wizard;
