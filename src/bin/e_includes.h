b1fec3fc67670a04a625826148c6428165986f65
Deon 'PrinceAMD' Thomas <PrinceAMD.Elive@Gmail.com>
2013-03-13 01:07:50 -0400

    Main: GPU/Video Card detection.
    This is used to get info about the computers video card
    Required by:
        Mode: Night
        Wizard: Composite gpu detection...

--- src/bin/e_includes.h
+++ src/bin/e_includes.h
@@ -43,6 +43,7 @@
 #include "e_actions.h"
 #include "e_popup.h"
 #include "e_gadcon_popup.h"
+#include "e_glxinfo.h"
 #include "e_ipc_codec.h"
 #include "e_test.h"
 #include "e_prefix.h"
--- src/bin/e_main.c
+++ src/bin/e_main.c
@@ -530,6 +530,11 @@ main(int argc, char **argv)
    TS("E_Env Init Done");
    _e_main_shutdown_push(e_env_shutdown);
 
+   TS("E_Glxinfo Init");
+   e_glxinfo_init();
+   TS("E_Glxinfo Init Done");
+   _e_main_shutdown_push(e_glxinfo_shutdown);
+
    e_util_env_set("E_ICON_THEME", e_config->icon_theme);
    ecore_exe_run_priority_set(e_config->priority);
    locked |= e_config->desklock_start_locked;
