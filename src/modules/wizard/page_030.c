a195e1c9d361e85e1fd29e4043c606ccd362b9db
Deon 'PrinceAMD' Thomas <PrinceAMD.Elive@Gmail.com>
2013-03-28 18:31:37 -0400

    Wizard: Update Enlightenment Menu Cache
    Before e17 starts detect all proper .desktop files and save them into
    the config.

--- src/modules/wizard/page_030.c
+++ src/modules/wizard/page_030.c
@@ -13,14 +13,40 @@ wizard_page_shutdown(E_Wizard_Page *pg __UNUSED__)
    return 1;
 }
 */
+
+static void
+_wizard_update_menu_cache(Efreet_Menu *menu)
+{
+   Eina_List *l;
+
+   if (menu->entries)
+     {
+        Efreet_Menu *entry;
+
+        EINA_LIST_FOREACH(menu->entries, l, entry)
+          {
+             if (entry->type == EFREET_MENU_ENTRY_DESKTOP)
+               e_int_menus_cache_update(entry->desktop);
+             else if (entry->type == EFREET_MENU_ENTRY_MENU)
+               _wizard_update_menu_cache(entry);
+          }
+     }
+}
+
 EAPI int
 wizard_page_show(E_Wizard_Page *pg __UNUSED__)
 {
+   Efreet_Menu *menu = NULL;
    char buf[PATH_MAX];
 
    snprintf(buf, sizeof(buf), "%s/etc/xdg/menus/enlightenment.menu",
             e_prefix_get());
    e_config->default_system_menu = eina_stringshare_add(buf);
+
+   menu = efreet_menu_get();
+
+   if (menu) _wizard_update_menu_cache(menu);
+   e_config_save_queue();
    return 0; /* 1 == show ui, and wait for user, 0 == just continue */
 }
 /*
