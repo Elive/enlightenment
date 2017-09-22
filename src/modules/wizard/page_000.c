cdd3aca121082d3108104f612605c2f5fabb5000
Thanatermesis <thanatermesis@gmail.com>
2017-09-23 01:42:25 +0200

    automatically configure scale without asking

--- src/modules/wizard/page_000.c
+++ src/modules/wizard/page_000.c
@@ -31,6 +31,27 @@ wizard_page_show(E_Wizard_Page *pg __UNUSED__)
 {
    Evas_Object *o;
 
+   // Scale configuration from here
+   int dpi;
+   double scale = 1.0;
+
+   dpi = ecore_x_dpi_get();
+   scale = ((float)dpi / 96);
+
+   // defaults
+   if (scale < 0.8) scale = 0.8;
+   if (scale > 3.0) scale = 3.0;
+
+   /*// TODO FIXME: temporal elive limitation to max 1.5 scaling: e17 is not so good to work with more than 1.5*/
+   if (scale > 1.5) scale = 1.5;
+
+   // set a good scale since the first page (ignored later in page 050)
+   e_config->scale.use_dpi = 0;
+   e_config->scale.use_custom = 1;
+   e_config->scale.factor = scale;
+   e_scale_update();
+
+
    e_wizard_title_set(_("Enlightenment"));
    e_wizard_button_next_enable_set(0);
    o = edje_object_add(pg->evas);
